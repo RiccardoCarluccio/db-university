e4fb196820f937a8d132a42c69da75a29c882255
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-09-14 17:05:53 +0200

    end tasks

--- db-university.sql
+++ db-university.sql
@@ -1,27 +1,40 @@
-/* Selezionare tutti gli studenti nati nel 1990 */
+/* 1 Selezionare tutti gli studenti nati nel 1990 */
 SELECT *
 FROM `students`
 WHERE YEAR(`date_of_birth`) = 1990;
 
-/* Selezionare tutti i corsi che valgono più di 10 crediti */
+/* 2 Selezionare tutti i corsi che valgono più di 10 crediti */
 SELECT *
 FROM `courses`
 WHERE `cfu` > 10;
 
-/* Selezionare tutti gli studenti con più di 30 anni */
+/* 3 Selezionare tutti gli studenti con più di 30 anni */
 SELECT *
 FROM `students`
-WHERE YEAR(CURRENT_DATE) - YEAR(`date_of_birth`) > 30;
+WHERE YEAR(CURDATE) - YEAR(`date_of_birth`) > 30;
 
-/* Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea */
+/* 4 Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea */
 SELECT * 
 FROM `courses` 
 WHERE `period` = "I semestre"
 AND `year` = 1;
 
-/* Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 */
+/* 5 Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 */
 SELECT * 
 FROM `exams`
 WHERE `hour` > "14:00"
 AND `date` = "2020-06-20";
 
+/* 6 Selezionare tutti i corsi di laurea magistrale */
+SELECT *
+FROM `degrees`
+WHERE `level` = "magistrale";
+
+/* 7 Da quanti dipartimenti è composta l'università? */
+SELECT *
+FROM `departments`;
+
+/* 8 Quanti sono gli insegnanti che non hanno un numero di telefono? */
+SELECT *
+FROM `teachers`
+WHERE `phone` is NULL;
