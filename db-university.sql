72b2f2aae7474933c179096b4e7f30f3904ab1c0
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-09-14 16:56:42 +0200

    5° task

--- db-university.sql
+++ db-university.sql
@@ -20,3 +20,8 @@ WHERE `period` = "I semestre"
 AND `year` = 1;
 
 /* Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 */
+SELECT * 
+FROM `exams`
+WHERE `hour` > "14:00"
+AND `date` = "2020-06-20";
+
