7610f23ca8cb22cc86efec244974af8d7d1c7bd6
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-09-18 10:55:48 +0200

    do JOIN task4

--- query-join.sql
+++ query-join.sql
@@ -1,11 +1,11 @@
-/* Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia */
+/* 1) Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia */
 SELECT `students`.`name` AS `Nome`, `students`.`surname` AS `Cognome`, `degrees`.`name` AS "Corso di Laurea"
 FROM `students`
 INNER JOIN `degrees`
 ON `students`.`degree_id` = `degrees`.`id`
 WHERE `degrees`.`name` = "Corso di Laurea in Economia";
 
-/* Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di Neuroscienze */
+/* 2) Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di Neuroscienze */
 SELECT `degrees`.`name` AS `Corso di Laurea`, `degrees`.`level` AS `Livello`, `departments`.`name` AS `Nome Dipartimento`
 FROM `degrees`
 INNER JOIN `departments`
@@ -13,7 +13,7 @@ ON `degrees`.`department_id` = `departments`.`id`
 WHERE `degrees`.`level` = "magistrale"
 AND `departments`.`id` = 7;
 
-/* Selezionare tutti i corsi in cui insegna Fulvio Amato (id = 44) */
+/* 3) Selezionare tutti i corsi in cui insegna Fulvio Amato (id = 44) */
 SELECT `teachers`.`name` AS `Nome Professore`, `teachers`.`surname` AS `Cognome Professore`, `courses`.`name` AS `Nome Corso`
 FROM `course_teacher`
 INNER JOIN `courses`
@@ -21,3 +21,13 @@ ON `courses`.`id` = `course_teacher`.`teacher_id`
 INNER JOIN `teachers`
 ON `teachers`.`id` = `course_teacher`.`course_id`
 WHERE `teachers`.`id` = 44;
+
+/* 4) Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome */
+SELECT `students`.`surname` AS `Cognome Studente`, `students`.`name` AS `Nome Studente`, `degrees`.`name` AS `Corso di Laurea`, `departments`.`name` AS `Nome Dipartimento`
+FROM `students`
+INNER JOIN `degrees`
+ON `degrees`.`id` = `students`.`degree_id`
+INNER JOIN `departments`
+ON `departments`.`id` = `degrees`.`department_id`
+ORDER BY `students`.`surname`, `students`.`name`;
+
