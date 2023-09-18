b08220393652073c7463ba126e1c44693922af88
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-09-18 10:23:59 +0200

    do JOIN TASK2

--- query-join.sql
+++ query-join.sql
@@ -6,3 +6,9 @@ ON `students`.`degree_id` = `degrees`.`id`
 WHERE `degrees`.`name` = "Corso di Laurea in Economia";
 
 /* Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di Neuroscienze */
+SELECT `degrees`.`name` AS `Corso di Laurea`, `degrees`.`level` AS `Livello`, `departments`.`name` AS `Nome Dipartimento`
+FROM `degrees`
+INNER JOIN `departments`
+ON `degrees`.`department_id` = `departments`.`id`
+WHERE `degrees`.`level` = "magistrale"
+AND `departments`.`id` = 7;
